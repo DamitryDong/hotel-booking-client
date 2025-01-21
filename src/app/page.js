6027d2ac4b52390cddc6da5d5e3a9d62467b6812
Damitry Dong <Damitry111@gmail.com>
2025-01-21 16:08:04 -0600

    Adjusted this for frontend purpose, Specifically temp removal of user auth and connection to database

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 'use client';
 
- // any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
+// any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
 
 import { Button } from 'react-bootstrap';
 import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
-import { useAuth } from '@/utils/context/authContext';
+// import { useAuth } from '@/utils/context/authContext';
 
 function Home() {
-  const { user } = useAuth();
+  // const { user } = useAuth();
 
   return (
     <div
@@ -19,10 +19,11 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
+      {/* <h1>Hello {user.displayName}! </h1> */}
+      <h1>YOU NEED TO PUT THE NAME BACK AND ALSO FIX THE LAYOUT PAGE WHEN YOU PAST THROUGH USER FIX ALL OF THEM PLEASSSSE </h1>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
+        Sign Out THIS WILL EROOR
       </Button>
     </div>
   );
