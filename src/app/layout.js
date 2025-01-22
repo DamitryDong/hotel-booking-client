53bc1053910402c7613cb7232ee07b485eeb5a49
Damitry Dong <Damitry111@gmail.com>
2025-01-21 21:35:33 -0600

    added auth back in

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,18 @@
 import { Inter } from 'next/font/google';
 import PropTypes from 'prop-types';
-// import ClientProvider from '@/utils/context/ClientProvider';
+import ClientProvider from '@/utils/context/ClientProvider';
 
 // IMPORT GOOGLE 'Rye" font later
 // USE GOOGLE "Poppins" font for all other
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css';
-import NavBar from '../components/NavBar'; // REMOVE THIS NAVBAR SINCE ITS USUALLY LOADED REGARDELSS BECAUSE OF VIEW DIRECTORY IN CONTEXT
 
 const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }) {
   return (
-    // <html lang="en">
-    //   <body className={inter.className}>
-    //     <ClientProvider>{children}</ClientProvider>
-    //   </body>
-    // </html>
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        {children}
+        <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
   );
