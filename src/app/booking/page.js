e73b518efd91fe4cc558c968447cae67ebfb3da4
Damitry Dong <Damitry111@gmail.com>
2025-02-13 14:14:21 -0600

    temp fix to the search bar

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -23,12 +23,18 @@ export default function BookingHome() {
   }, []);
 
   const handleSearch = () => {
-    const filter = filteredItems.filter((bookingItem) => bookingItem.room_size.includes(searchTheWord.toLowerCase()));
-    setFilteredItems(filter);
+    if (searchTheWord) {
+      const searchNumber = Number(searchTheWord);
+      const filter = filteredItems.filter((bookingItem) => bookingItem.id === searchNumber);
+      setFilteredItems(filter);
+    } else {
+      getAllBookings().then((bookings) => setFilteredItems(bookings));
+      getAllCustomers().then((customers) => setCustomerItems(customers));
+      getAllCustomerJoinBookings().then((joinedBooking) => setcustomerBookingItem(joinedBooking));
+    }
   };
 
   const handleDeleteBooking = (deletedBookingId) => {
-    console.log('Deleting booking ID:', deletedBookingId);
     setFilteredItems((prevItems) => prevItems.filter((booking) => booking.id !== deletedBookingId));
   };
 
@@ -36,14 +42,19 @@ export default function BookingHome() {
     <div>
       <div className="text-center d-flex flex-column align-items-center" style={{ marginBottom: '2%' }}>
         <h2 style={{ paddingBottom: '2px', paddingTop: '2px', marginBottom: '2%', background: '#502d1b', width: '70%', borderRadius: '10px', color: 'white' }}> Bookings </h2>
-        <Form className="d-flex mb-3" style={{ maxWidth: '400px', width: '100%' }}>
-          <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" value={searchTheWord} onChange={(e) => setsearchTheWord(e.target.value)} />
+
+        <div className="RoomsOnBookingInfo" style={{ width: '60%' }}>
+          <RoomPlanForShow onbookingDeleteTrigger={filteredItems} RoomToHightlight={RoomHighlighted} />
+        </div>
+
+        <Form className="d-flex mb-3" style={{ maxWidth: '400px', width: '100%', marginTop: '1%' }}>
+          <Form.Control type="search" placeholder="Search ID" className="me-2" aria-label="Search" value={searchTheWord} onChange={(e) => setsearchTheWord(e.target.value)} />
           <Button onClick={handleSearch} variant="outline-success">
             Search
           </Button>
         </Form>
 
-        <div className="text-center d-flex flex-column align-items-center" style={{ marginBottom: '2%', width: '90%', overflowX: 'auto', overflowY: 'hidden', height: '26vh', backgroundColor: 'rgba(0, 0, 0, 0.11)' }}>
+        <div className="text-center d-flex flex-column align-items-center" style={{ width: '90%', overflowX: 'auto', overflowY: 'hidden', height: '27vh', backgroundColor: 'rgba(0, 0, 0, 0.11)' }}>
           <Row className="g-3" style={{ display: 'flex', flexWrap: 'nowrap', width: '90%' }}>
             {filteredItems.map((booking) => (
               <Col key={booking.id} sm={3} style={{ flex: '0 0 auto' }}>
@@ -52,10 +63,6 @@ export default function BookingHome() {
               </Col>
             ))}
           </Row>
-        </div>
-
-        <div className="RoomsOnBookingInfo" style={{ width: '60%' }}>
-          <RoomPlanForShow onbookingDeleteTrigger={filteredItems} RoomToHightlight={RoomHighlighted} />
         </div>
       </div>
     </div>
